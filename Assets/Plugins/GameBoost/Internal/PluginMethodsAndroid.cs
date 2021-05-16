74214b42ba3e4c4db37ddf7ca8d8ba85b1f3993a
chestnut42 <andrey.makarich@gmail.com>
2021-05-16 18:49:38 -0400

    add android sdk aar (#1)
    * add android sdk aar
    
    * update aar
    
    * wip
    
    * wip
    
    * android lib added
    
    * apply comments
    
    Co-authored-by: shalom.aviv <shalom.aviv@protonmail.com>

---
+++ Assets/Plugins/GameBoost/Internal/PluginMethodsAndroid.cs
@@ -0,0 +1,76 @@
+#if UNITY_ANDROID && !UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace Plugins.GameBoost
+{
+    internal class PluginMethodsAndroid : IPluginMethods
+    {
+        private AndroidJavaClass ajc;
+ 
+        private AndroidJavaClass plugin() {
+            if (ajc != null) 
+            {
+                return ajc;
+            }
+
+            try
+            {
+                ajc = new AndroidJavaClass("com.doitbetter.sdk.u3d.U3DGameBoostSDK");
+            }
+            catch (Exception exception)
+            {
+                GBLog.LogError($"Exception during Initialize PluginMethodsAndroid: {exception}");
+            }
+            return ajc;
+        }   
+
+        public void InitializeWith(string apiKey)
+        {
+            if (plugin() != null)
+            {
+                try
+                {
+                    plugin().CallStatic("initialize", apiKey);
+                }
+                catch (Exception exception)
+                {
+                    GBLog.LogError($"Exception during CallStatic(initialize): {exception}");
+                }
+            }
+        }
+
+        public void SendEvent(string eventName, string jsonString, string deduplicateId)
+        {
+            if (plugin() != null)
+            {
+                try
+                {
+                    plugin().CallStatic("sendEvent", eventName, jsonString, deduplicateId);
+                }
+                catch (Exception exception)
+                {
+                    GBLog.LogError($"Exception during CallStatic(sendEvent): {exception}");
+                }
+            }            
+        }
+
+        public void SetLoggingEnabled(bool isLoggingEnabled)
+        {
+            if (plugin() != null)
+            {
+               try
+                {
+                    plugin().CallStatic("enableLogging", isLoggingEnabled);
+                }
+                catch (Exception exception)
+                {
+                    GBLog.LogError($"Exception during CallStatic(enableLogging): {exception}");
+                }
+            }
+        }
+    }
+}
+#endif
