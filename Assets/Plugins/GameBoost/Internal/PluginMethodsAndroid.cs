a81d218e4a60d186273295e9b46fe95f980219e2
Andrei <andrey.makarich@gmail.com>
2021-05-17 21:46:37 -0400

    android plugin methods cleanup

--- Assets/Plugins/GameBoost/Internal/PluginMethodsAndroid.cs
+++ Assets/Plugins/GameBoost/Internal/PluginMethodsAndroid.cs
@@ -1,75 +1,30 @@
 #if UNITY_ANDROID && !UNITY_EDITOR
 using UnityEngine;
-using System.Collections;
-using System;
-using System.Collections.Generic;
 
 namespace Plugins.GameBoost
 {
     internal class PluginMethodsAndroid : IPluginMethods
     {
-        private AndroidJavaClass ajc;
- 
-        private AndroidJavaClass plugin() {
-            if (ajc != null) 
-            {
-                return ajc;
-            }
+        private AndroidJavaClass androidJavaClass;
 
-            try
-            {
-                ajc = new AndroidJavaClass("com.doitbetter.sdk.u3d.U3DGameBoostSDK");
-            }
-            catch (Exception exception)
-            {
-                GBLog.LogError($"Exception during Initialize PluginMethodsAndroid: {exception}");
-            }
-            return ajc;
-        }   
+        public PluginMethodsAndroid()
+        {
+            androidJavaClass = new AndroidJavaClass("com.doitbetter.sdk.u3d.U3DGameBoostSDK");
+        }
 
         public void InitializeWith(string apiKey)
         {
-            if (plugin() != null)
-            {
-                try
-                {
-                    plugin().CallStatic("initialize", apiKey);
-                }
-                catch (Exception exception)
-                {
-                    GBLog.LogError($"Exception during CallStatic(initialize): {exception}");
-                }
-            }
+            androidJavaClass.CallStatic("initialize", apiKey);
         }
 
         public void SendEvent(string eventName, string jsonString, string deduplicateId)
         {
-            if (plugin() != null)
-            {
-                try
-                {
-                    plugin().CallStatic("sendEvent", eventName, jsonString, deduplicateId);
-                }
-                catch (Exception exception)
-                {
-                    GBLog.LogError($"Exception during CallStatic(sendEvent): {exception}");
-                }
-            }            
+            androidJavaClass.CallStatic("sendEvent", eventName, jsonString, deduplicateId);
         }
 
         public void SetLoggingEnabled(bool isLoggingEnabled)
         {
-            if (plugin() != null)
-            {
-               try
-                {
-                    plugin().CallStatic("enableLogging", isLoggingEnabled);
-                }
-                catch (Exception exception)
-                {
-                    GBLog.LogError($"Exception during CallStatic(enableLogging): {exception}");
-                }
-            }
+            androidJavaClass.CallStatic("enableLogging", isLoggingEnabled);
         }
     }
 }
