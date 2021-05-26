ebd3964badb4b9c6d0dc9c01504d268a1b8b9667
chestnut42 <andrey.makarich@gmail.com>
2021-05-25 20:53:12 -0400

    Dev/android 2 (#4)
    First version of android support

---
+++ Assets/Plugins/GameBoost/Internal/PluginMethodsAndroid.cs
@@ -0,0 +1,31 @@
+#if UNITY_ANDROID && !UNITY_EDITOR
+using UnityEngine;
+
+namespace Plugins.GameBoost
+{
+    internal class PluginMethodsAndroid : IPluginMethods
+    {
+        private AndroidJavaClass androidJavaClass;
+
+        public PluginMethodsAndroid()
+        {
+            androidJavaClass = new AndroidJavaClass("com.doitbetter.sdk.u3d.U3DGameBoostSDK");
+        }
+
+        public void InitializeWith(string apiKey)
+        {
+            androidJavaClass.CallStatic("initialize", apiKey);
+        }
+
+        public void SendEvent(string eventName, string jsonString, string deduplicateId)
+        {
+            androidJavaClass.CallStatic("sendEvent", eventName, jsonString, deduplicateId);
+        }
+
+        public void SetLoggingEnabled(bool isLoggingEnabled)
+        {
+            androidJavaClass.CallStatic("enableLogging", isLoggingEnabled);
+        }
+    }
+}
+#endif
