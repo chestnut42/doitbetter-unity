9e4b7698cf826090e97db66b9b2575b951ea998b
shalom.aviv <shalom.aviv@protonmail.com>
2021-10-05 01:26:56 +0300

    added valid Api path. coroutine refactoring (not tested yest)

--- Assets/Plugins/GameBoost/Internal/PluginEventTracker.cs
+++ Assets/Plugins/GameBoost/Internal/PluginEventTracker.cs
@@ -28,17 +28,18 @@ namespace Plugins.GameBoost
             var jsonString = jsonSerializer.Serialize(eventData, false);
             pluginMethods.SendEvent(eventName, jsonString, deduplicateId);
         }
-
-        public IEnumerator Level(string room_number, Action<BusData.LevelData> callMethod)
+        
+        public AsyncResult<string, BusData.LevelData> LevelRequest(string room_number)
         {
-            return pluginMethods.Level(room_number, callMethod);
-        }
-
-        public IEnumerator Abilities(string reason, string room_number, Action<BusData.AbilitiesData> callMethod)
+            return new AsyncResult<string, BusData.LevelData>(room_number, this.Level);
+        }            
+        
+        public AsyncResult<Tuple<string, string>, BusData.AbilitiesData> AbilitiesRequest(string reason, string room_number)
         {
-            return pluginMethods.Abilities(reason, room_number, callMethod);
-        }
-
+            var parameters = new Tuple<string, string>(reason, room_number);
+            return new AsyncResult<Tuple<string, string>, BusData.AbilitiesData>(parameters, this.Abilities);
+        }                    
+        
         public void AddKeyHash(string keyValue, string hashValue)
         {
             var adoptedHash = hashValue.Replace('+', '-').Replace('/', '_');
@@ -47,5 +48,44 @@ namespace Plugins.GameBoost
                 pluginMethods.AddKeyHash(keyValue, adoptedHash);
             }
         }
+        
+        private IEnumerator Level(string room_number, Action<BusData.LevelData> callMethod)
+        {
+            return pluginMethods.Level(room_number, callMethod);
+        }
+
+        private IEnumerator Abilities(Tuple<string, string> parameters, Action<BusData.AbilitiesData> callMethod)
+        {
+            return pluginMethods.Abilities(parameters.Item1, parameters.Item2, callMethod);
+        }
+
+    }
+    
+    class AsyncResult<Params, Result>
+    {
+        public delegate IEnumerator AsyncCommand(Params parameters, Action<Result> callMethod);
+        
+        private Params parameters;
+        private Result commandResult;
+        private Boolean isDone;
+        private AsyncCommand command;
+        
+        public Result CommandResult => commandResult;
+        public Boolean IsDone => isDone;
+        
+        public AsyncResult(Params parameters, AsyncCommand command)
+        {
+            this.parameters = parameters;
+            this.command = command;
+        }
+        public IEnumerator Run()
+        {
+            if (!isDone) {
+                yield return null;
+                command(parameters, result => commandResult = result);
+                isDone = true;                 
+            }
+        }
+
     }
 }
